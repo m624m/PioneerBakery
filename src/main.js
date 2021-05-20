aea62e893adcd146ba4127c300579deccc6f186d
m624m <81347295+m624m@users.noreply.github.com>
2021-05-20 13:17:59 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,12 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/js/all.js";
-// import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-// import "bootstrap/dist/css/bootstrap.css";
-// import "bootstrap-vue/dist/bootstrap-vue.css";
-
-// Vue.use(BootstrapVue);
-// Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false;
 
